13be06f945b7777b13378a308fbde85eb6b7858d
syobochim <syobochim@gmail.com>
2023-03-17 19:03:05 +0900

    Initial commit

---
+++ test/ai_gon-server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiGonServer from '../lib/ai_gon-server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai_gon-server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiGonServer.AiGonServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
